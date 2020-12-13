b1d371e2d1d649889591c3d203b13bf9ee3f6d51
KolpaschikovDenis <kolpashikov.denis@gmail.com>
2020-12-13 18:39:01 +0400

    Финальный коммит №2

--- src/psb/kdv/Main.java
+++ src/psb/kdv/Main.java
@@ -53,11 +53,17 @@ public class Main {
         Collections.reverse(countsList);
 
         System.out.println(mwbList.get(0));
-        String header = "Articul;Name;Leftover;CostPrice;Brand;Barcode;ArticulWB;LeftoverWBMH;CostAmount\n";
+//        String header = "Articul;Name;Leftover;CostPrice;Brand;Barcode;ArticulWB;LeftoverWBMH;CostAmount\n";
         String header1= "Articul;Name;Leftover;CostPrice;CostAmount;Brand;ArticulWB;LeftoverWBWH;Date\n";
-        String strNoData = "Нет";
 
-        StringBuilder stringBuilder = new StringBuilder(header);
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        String strDate = dateFormat.format(date);
+        StringBuilder stringBuilder = new StringBuilder(header1);
+        StringBuilder sbb = new StringBuilder();
+        StringBuilder sbbb = new StringBuilder();
+        boolean b = false;
+        boolean bb = false;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(mwbList.get(0))), "windows-1251"));
         String line = br.readLine();
@@ -70,8 +76,6 @@ public class Main {
             double CostAmount = Double.parseDouble(s[4].trim());
             BufferedReader brr = new BufferedReader(new InputStreamReader(new FileInputStream(new File(barList.get(0))), "windows-1251"));
             String lline = brr.readLine();
-            boolean b = false;
-            boolean bb = false;
             while((lline = brr.readLine()) != null){
                 String ss[] = lline.split(";");
                 String Brand = ss[0].trim();
@@ -79,6 +83,8 @@ public class Main {
                 String NomenclatureWBMW = ss[2].trim();
                 String Barcode = ss[3].trim().length() > 0 ? ss[3].trim() : "Нет данных";
                 if(Articul.equals(NomenclatureWBMW)){
+                    b = true;
+                    sbb.append(Articul).append(";").append(Name).append(";").append(Leftover).append(";").append(CostPrice).append(";");
                     BufferedReader brrr = new BufferedReader(new InputStreamReader(new FileInputStream(new File(countsList.get(0))), "windows-1251"));
                     String llline = brrr.readLine();
                     while ((llline = brrr.readLine()) != null) {
@@ -87,27 +93,37 @@ public class Main {
                         String AArticulWB = sss[1].trim();
                         double LeftoverWBWH = Double.parseDouble(sss[2].trim());
                         if (ArticulWB.equals(AArticulWB) && Brand.equals(BBrand)) {
-                            CostAmount = CostAmount + LeftoverWBWH * CostPrice;
-
-                            Date date = Calendar.getInstance().getTime();
-                            DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-                            String strDate = dateFormat.format(date);
-
-                            stringBuilder.append(Articul).append(";").append(Name).append(";").append(Leftover).append(";")
-                                    .append(CostPrice).append(";").append(CostAmount).append(";").append(BBrand).append(";")
-                                    .append(AArticulWB).append(";").append(LeftoverWBWH).append(";").append(strDate).append("\n");
+                            bb = true;
+                            CostAmount = (Leftover + LeftoverWBWH) * CostPrice;
+                            sbbb.append(CostAmount).append(";").append(BBrand).append(";").append(AArticulWB).append(";")
+                                    .append(LeftoverWBWH).append("\n");
+//
+//                            stringBuilder.append(Articul).append(";").append(Name).append(";").append(Leftover).append(";")
+//                                    .append(CostPrice).append(";").append(CostAmount).append(";").append(BBrand).append(";")
+//                                    .append(AArticulWB).append(";").append(LeftoverWBWH).append(";").append(strDate).append("\n");
                             bb = true;
                         }
                     }
+                    if(!bb){
+                        sbbb.append("Нет данных;Нет данных;Нет данных;Нет данных;").append(strDate).append("\n");
+                    }
+                    sbb.append(sbbb);
+                    sbbb.setLength(0);
+                    bb = false;
                 }
             }
-            bb = false;
+            if(!b){
+                sbb.append(Articul).append(";Нет данных;Нет данных;Нет данных;Нет данных;Нет данных;Нет данных;Нет данных;")
+                        .append(strDate).append("\n");
+            }
+            stringBuilder.append(sbb);
+            sbb.setLength(0);
             b = false;
         }
         System.out.println(stringBuilder.toString());
-        Date date = Calendar.getInstance().getTime();
-        DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss");
-        String strDate = dateFormat.format(date);
+        date = Calendar.getInstance().getTime();
+        dateFormat = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+        strDate = dateFormat.format(date);
         File fout = new File(String.valueOf(finalsDir)+"\\Consolidated"+strDate+".csv");
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fout), "windows-1251"));
         bw.write(stringBuilder.toString());
